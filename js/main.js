f63d33744fb0a4bf1fb31f269ed7de87f00d84a3
bgcrafts <bartosz.giecewicz@gmail.com>
2021-02-10 18:49:43 +0100

    zadanie32

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let num1 = 2;
+let num2 = 5;
+
+function akcja(num1, num2) {
+
+    if (num1 > num2) {
+        return num1;
+    }
+    else if (num1 < num2) {
+        return num2;
+    }
+
+}
+
+console.log(akcja(num1, num2));
